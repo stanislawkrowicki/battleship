22acb3a1a961223bd939e2f6a5c01487c8067bc7
ThePixelOne <dawid.grabczewski@gmail.com>
2024-10-26 22:00:06 +0200

    title screen (powali mnie coś)

--- client/src/main/java/com/put/battleship/client/HelloApplication.java
+++ client/src/main/java/com/put/battleship/client/HelloApplication.java
@@ -3,6 +3,7 @@ package com.put.battleship.client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("title_screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 563);
+
+        Image icon = new Image(getClass().getResource("battleship.jpg").toExternalForm());
+        stage.getIcons().add(icon);
+        stage.setTitle("Bajtelszips :D");
         stage.setScene(scene);
         stage.show();
     }
