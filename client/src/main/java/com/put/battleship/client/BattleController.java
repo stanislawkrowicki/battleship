a00b29d37b94530a531c07c2cd586aacb5a85a20
ThePixelOne <dawid.grabczewski@gmail.com>
2024-12-29 18:03:36 +0100

    Początkowe tłumaczenie ustawień i strzelania na gui

--- client/src/main/java/com/put/battleship/client/BattleController.java
+++ client/src/main/java/com/put/battleship/client/BattleController.java
@@ -7,38 +7,105 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.util.*;
+
+
 import java.io.IOException;
 
 public class BattleController {
 
-    @FXML
-    private HBox hBoxVirtual;
-
     private static final int RECTANGLE_WIDTH = 50;
     private static final int RECTANGLE_HEIGHT = 50;
     private static final int SPACING_BETWEEN_GRIDS = 100;
+    private final Map<String, Object> carrier = new HashMap<>();
+    private final boolean canSetShips = false;
+    int[][] yourBoardMatrix = new int[10][10];
+    int[][] enemyBoardMatrix = new int[10][10];
+    SceneController sceneController = new SceneController();
+    private boolean canAttack = true;
+    @FXML
+    private HBox hBoxVirtual;
 
     @FXML
-    public void initialize(){
-        GridPane gridPane1 = createGrid(Color.YELLOW);
-        GridPane gridPane2 = createGrid(Color.LIGHTGREEN);
-
-        hBoxVirtual.getChildren().addAll(gridPane1, gridPane2);
+    public void initialize() {
+        GridPane yourBoard = createGrid(Color.YELLOW, false);
+        GridPane enemyBoard = createGrid(Color.LIGHTGREEN, true);
+        createCarrier();
+        translateToMatrix();
+        fillYourGrid(yourBoard);
+        hBoxVirtual.getChildren().addAll(yourBoard, enemyBoard);
     }
-    private GridPane createGrid(Color color){
+
+    private GridPane createGrid(Color color, boolean isEnemy) {
         GridPane gridPane = new GridPane();
         for (int row = 0; row < 10; row++) {
             for (int col = 0; col < 10; col++) {
                 Rectangle rectangle = new Rectangle(RECTANGLE_WIDTH, RECTANGLE_HEIGHT);
-
+                final int currentRow = row;
+                final int currentCol = col;
                 rectangle.setFill(color.darker().interpolate(color.brighter(), Math.random()));
-
+                if (isEnemy) {
+                    rectangle.setOnMouseClicked(event -> {
+                        if (canAttack) {
+                            handleAttack(currentCol, currentRow, rectangle);
+                        }
+                    });
+                }
                 gridPane.add(rectangle, col, row);
             }
         }
+
         return gridPane;
     }
-    SceneController sceneController = new SceneController();
+
+    public void createCarrier() {
+        carrier.put("type", "carrier");
+        List<int[]> positions = new ArrayList<>();
+        positions.add(new int[]{0, 4});
+        positions.add(new int[]{1, 4});
+        positions.add(new int[]{2, 4});
+        positions.add(new int[]{3, 4});
+        carrier.put("positions", positions);
+    }
+
+    public void translateToMatrix() {
+        List<int[]> retrievedPositions = (List<int[]>) carrier.get("positions");
+        for (int[] position : retrievedPositions) {
+            yourBoardMatrix[position[0]][position[1]] = 1;
+            enemyBoardMatrix[position[0]][position[1]] = 1;
+        }
+    }
+
+    public void fillYourGrid(GridPane yourGrid) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (yourBoardMatrix[i][j] == 1) {
+                    for (javafx.scene.Node node : yourGrid.getChildren()) {
+                        if (GridPane.getRowIndex(node) == i && GridPane.getColumnIndex(node) == j) {
+                            Rectangle rectangle = (Rectangle) node;
+                            rectangle.setFill(Color.ORANGE);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void handleAttack(int col, int row, Rectangle rectangle) {
+        if (enemyBoardMatrix[GridPane.getRowIndex(rectangle)][GridPane.getColumnIndex(rectangle)] == 1)
+            rectangle.setFill(Color.RED);
+        else {
+            rectangle.setFill(Color.BLUE);
+            canAttack = false;
+            enemyAttack();
+        }
+    }
+
+    private void enemyAttack() {
+        //tutaj czekamy na szczylanie przeciwnika
+        canAttack = true;
+    }
+
     public void switchToEndScreen(ActionEvent event) throws IOException {
         sceneController.switchScene(event, "end_screen.fxml");
 
--- client/src/main/java/com/put/battleship/client/SceneController.java
+++ client/src/main/java/com/put/battleship/client/SceneController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,23 +15,21 @@ public class SceneController {
 
     public void switchScene(ActionEvent event, String fxmlFile) throws IOException {
         root = FXMLLoader.load(getClass().getResource(fxmlFile));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.getScene().setRoot(root);
         stage.show();
 
     }
-    public void switchToLoadingScreen(ActionEvent event) throws IOException{
+
+    public void switchToLoadingScreen(ActionEvent event) throws IOException {
         switchScene(event, "loading_screen.fxml");
     }
 
-    public void switchToBattleScreen(ActionEvent event) throws IOException{
-
-
+    public void switchToBattleScreen(ActionEvent event) throws IOException {
         switchScene(event, "battle_screen.fxml");
-
-
     }
-    public void switchToTitleScreen(ActionEvent event) throws IOException{
+    
+    public void switchToTitleScreen(ActionEvent event) throws IOException {
         switchScene(event, "title_screen.fxml");
 
     }
