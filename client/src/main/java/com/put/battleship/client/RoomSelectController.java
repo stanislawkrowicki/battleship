be2a66670f58a1cb8722b3cf04d2f2c9176e3f3f
ThePixelOne <dawid.grabczewski@gmail.com>
2025-01-08 13:26:54 +0100

    room code handling / title screen changes

---
+++ client/src/main/java/com/put/battleship/client/RoomSelectController.java
@@ -0,0 +1,46 @@
+package com.put.battleship.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class RoomSelectController {
+
+    SceneController sceneController = new SceneController();
+    String roomCode;
+
+    @FXML
+    private TextField roomCodeField;
+
+    @FXML
+    private Label errorLabel;
+
+    public boolean isCorrectFormat(String code) {
+        return code.length() <= 8 && code.length() >= 4;
+    }
+
+    public void createRoom(ActionEvent event) throws IOException {
+        roomCode = roomCodeField.getText();
+        if (isCorrectFormat(roomCode)) {
+            BattleShipsApp.model.setRoomCode(roomCode);
+            sceneController.switchScene(event, "loading_screen.fxml");
+        } else {
+            errorLabel.setText("Enter correct code format :)");
+
+        }
+    }
+
+    public void joinRoom(ActionEvent event) throws IOException {
+        roomCode = roomCodeField.getText();
+        if (isCorrectFormat(roomCode)) {
+            BattleShipsApp.model.setRoomCode(roomCode);
+            sceneController.switchScene(event, "loading_screen.fxml");
+        } else {
+            errorLabel.setText("Enter correct code format :)");
+
+        }
+    }
+}
